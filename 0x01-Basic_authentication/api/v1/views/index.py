48ecd98a0e379b1a47613045524adfc44e96d0df
Wakadaisho <paulwahome9@gmail.com>
2024-01-15 21:21:54 +0300

    add GET method to routes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,17 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized/", strict_slashes=False)
+@app_views.route("/unauthorized/", methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
       - user unauthorized
     """
+    abort(401)
 
 
-@app_views.route("/forbidden/", strict_slashes=False)
+@app_views.route("/forbidden/", methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
     Return:
       - user forbidden
     """
+    abort(403)
