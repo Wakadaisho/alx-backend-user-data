6e6f7ccf9ba4831e51261267d119aabd4b288b4e
Wakadaisho <paulwahome9@gmail.com>
2024-01-24 11:33:04 +0300

    Add login validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,14 @@ class Auth:
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(password))
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Method that checks if the password is valid
+        """
+        import bcrypt
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+        
